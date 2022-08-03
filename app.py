cbfd2142ec573dfc591d2b68cf84d34ce284adab
Taisiia Dzhaparbekova <taisiiadzhaparbekova@MacBook-Air-Taisiia.local>
2022-08-03 16:12:10 +0600

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+
+from flask import Flask, render_template, request, redirect
+from peewee import * 
+from models import Post
+
+app = Flask(__name__)
+
+@app.route('/') 
+def mike():
+    all_posts = Post.select()
+    return render_template("index.html", posts=all_posts)
+
+@app.route('/create', methods = ('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['title']
+
+        Post.create(
+            title = title,
+            description = description
+        )
+        return redirect('/')
+    return render_template('create.html')
+
+@app.route('/bye')
+def ex():
+    return '<h1>bye</h1>'
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
